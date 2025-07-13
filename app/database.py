fac02f9e7ad58090be5d8c29d9ca5736f8c80ce0
Chibuike Obiora <obiorachibuike22@gmail.com>
2025-07-13 19:45:42 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,9 +1,20 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:yourpassword@localhost:5432/kpa_db"
+# Load environment variables from .env file
+load_dotenv()
 
+# Use DATABASE_URL from .env
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Create the SQLAlchemy engine
 engine = create_engine(DATABASE_URL)
+
+# Create a configured "Session" class
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base class for model definitions
 Base = declarative_base()
